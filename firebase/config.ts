6e499f610531bb1c4ad546f6c0bade32e8304432
Zach Riane Machacon <zachriane01@gmail.com>
2023-06-19 12:52:32 +0800

    [CHORE] Moved Firebase config to env

--- firebase/config.ts
+++ firebase/config.ts
@@ -10,13 +10,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBnw1v0b-hBKprRbSTrM-rMF2FnvTYTnnY",
-  authDomain: "gdsc-cert-generator.firebaseapp.com",
-  projectId: "gdsc-cert-generator",
-  storageBucket: "gdsc-cert-generator.appspot.com",
-  messagingSenderId: "250996670250",
-  appId: "1:250996670250:web:c081f8b7be099d84c16853",
-  measurementId: "G-2GWF019CD5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
